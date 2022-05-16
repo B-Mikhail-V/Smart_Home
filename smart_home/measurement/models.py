83469f0ff4ad84068d151ff7b2fcc0bac129a76d
B-Mikhail-V <bakulin.mv@gmail.com>
2022-05-16 22:30:30 +0300

    1111

--- smart_home/measurement/models.py
+++ smart_home/measurement/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
 # TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+
+class Sensor(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.CharField(max_length=128)
+
+class Measurement(models.Model):
+    id = models.IntegerField()
+    name = models.ForeignKey(Sensor, on_delete=models.CASCADE)
+    temperature = models.DecimalField(max_digits=3, decimal_places=1)
+    date = models.DateTimeField()

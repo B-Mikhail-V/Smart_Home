ff9dfd764df499eb67f93459ab75c0c467cbd933
B-Mikhail-V <bakulin.mv@gmail.com>
2022-05-18 22:43:32 +0300

    Final_Image

--- smart_home/measurement/models.py
+++ smart_home/measurement/models.py
@@ -11,3 +11,4 @@ class Measurement(models.Model):
     name = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name='sensor_name', verbose_name='Sensor model')
     temperature = models.DecimalField(max_digits=3, decimal_places=1)
     created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(max_length=None, blank=True, null=True, upload_to='images/')
--- smart_home/measurement/serializers.py
+++ smart_home/measurement/serializers.py
@@ -13,13 +13,13 @@ class SensorSerializer(serializers.ModelSerializer):
 class MeasurementSerializer(serializers.ModelSerializer):
     class Meta:
         model = Measurement
-        fields = ['pk', 'name', 'temperature']
+        fields = ['pk', 'name', 'temperature', 'image']
 
 
 class MeasurementInfoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Measurement
-        fields = ['temperature', 'created_at']
+        fields = ['temperature', 'created_at', 'image']
 
 class SensorDetailSerializer(serializers.ModelSerializer):
     sensor_name = MeasurementInfoSerializer(read_only=True, many=True)
